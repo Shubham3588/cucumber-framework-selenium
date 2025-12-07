c1f1c9d638e488741c97745c6b5b7e806c4497c4
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2025-12-07 20:06:05 +0530

    Adding Jenkins File

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,34 +1,31 @@
 package utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
-	
-	private static Properties properties;
-	
-	static {
-	try {
-		
-		FileInputStream  input = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\config.properties");
-		properties = new Properties();
-		properties.load(input);
-		
-		
-	}catch(IOException e) {
-		e.printStackTrace();
-		throw new RuntimeException("Failed Loading properties file");
-	}
-	
-	
-	
-	
 
-}
-	public static String getProperty(String key) {
-		return properties.getProperty(key);
-	
-}
-}
+    private static Properties properties = new Properties();
 
+    static {
+        try {
+            // Load config.properties from classpath (src/test/resources)
+            InputStream input = ConfigReader.class.getClassLoader()
+                    .getResourceAsStream("config.properties");
+
+            if (input == null) {
+                throw new RuntimeException("config.properties not found in classpath");
+            }
+
+            properties.load(input);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed Loading properties file", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
